b0670f9597ec89b822da281194b6ebad008c10d3
astral4 <88992929+astral4@users.noreply.github.com>
2022-10-03 13:28:03 -0400

    try docs template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        remarkPlugins: [
-            'remark-math',
-        ],
-        rehypePlugins: [
-            ['rehype-katex', {
-            // Katex plugin options
-            }]
-        ]
-    }
+	integrations: [
+		// Enable Preact to support Preact JSX components.
+		preact(),
+		// Enable React for the Algolia search component.
+		react(),
+	],
+	site: `http://astro.build`,
 });
