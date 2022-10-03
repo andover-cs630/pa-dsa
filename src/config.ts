2ebde0b60ac382ffa3dc0237868932634450aa1b
astral4 <88992929+astral4@users.noreply.github.com>
2022-10-03 15:12:57 -0400

    remove i18n features

--- src/config.ts
+++ src/config.ts
@@ -20,31 +20,21 @@ export type Frontmatter = {
 	description: string;
 	layout: string;
 	image?: { src: string; alt: string };
-	dir?: 'ltr' | 'rtl';
 	ogLocale?: string;
-	lang?: string;
 };
-
-export const KNOWN_LANGUAGES = {
-	English: 'en',
-} as const;
-export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 export type Sidebar = Record<
-	typeof KNOWN_LANGUAGE_CODES[number],
-	Record<string, { text: string; link: string }[]>
+	string, { text: string; link: string }[]
 >;
 export const SIDEBAR: Sidebar = {
-	en: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
-	},
+	'Section Header': [
+		{ text: 'Introduction', link: 'introduction' },
+		{ text: 'Page 2', link: 'page-2' },
+		{ text: 'Page 3', link: 'page-3' },
+	],
+	'Another Section': [{ text: 'Page 4', link: 'page-4' }],
 };
