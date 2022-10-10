0ab3ef735d3fb78ccfc84b62f851246517f6386e
astral4 <88992929+astral4@users.noreply.github.com>
2022-10-10 13:43:15 -0400

    add new articles to sidebar

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,9 @@ export const SIDEBAR: Sidebar = {
 	],
 	'Article Demos': [
 		{ text: 'Introduction to Graphs', link: 'introduction-to-graphs' },
+		{ text: 'Hash Tables', link: 'hash-tables' },
 		{ text: 'Heapsort', link: 'heapsort' },
+		{ text: 'Quicksort', link: 'quicksort' },
 		{ text: 'Inline Code Snippets', link: 'code-snippets' },
 		{ text: 'Inline Images', link: 'inline-images' },
 	],
