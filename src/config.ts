6a1739bc29b44c89d14633dec6812e617f397048
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2022-10-31 08:46:35 -0400

    updated sidebar

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const SIDEBAR: Sidebar = {
 
 
 	'Algorithms': [
-		{ text: 'Heapsort', link: 'heapsort' },
+		{ text: 'Heapsort', link: 'Heapsort' },
 		{ text: 'Quicksort', link: 'quicksort' },
 		{ text: 'Lower Bounds of Sorting', link: 'lower-bounds-of-sorting' },
 		{ text: 'Graphing Algorithms', link: 'graphing-algorithms' },
