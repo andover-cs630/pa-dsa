9b7e8d864444d235e87f3a8e5caa8682f8efcfb7
astral4 <88992929+astral4@users.noreply.github.com>
2022-11-09 09:32:42 -0500

    styling active section

--- src/components/RightSidebar/TableOfContents.svelte
+++ src/components/RightSidebar/TableOfContents.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import type { MarkdownHeading } from "astro";
 
     export let headings: MarkdownHeading[];
+
+    type ItemOffset = {
+        id: string,
+        topOffset: number
+    };
+
+    let scrollY: number;
+    let titles: NodeListOf<Element>;
+    let itemOffsets: ItemOffset[];
+
+    onMount(() => {
+        titles = document.querySelectorAll('article :is(h1, h2, h3, h4, h5, h6)');
+        itemOffsets = Array.from(titles).map(title => ({
+            id: title.id,
+            topOffset: title.getBoundingClientRect().top + window.scrollY
+        }));
+    })
 </script>
+
+<svelte:window bind:scrollY={scrollY} />
 
 <h2 class="heading">On this page</h2>
 <ol>
@@ -12,10 +32,25 @@
         </a>
     </li>
     {#each headings as heading}
-        <li class="heading-link depth-{heading.depth}">
+        <li class="heading-link depth-{heading.depth}"
+            class:active={(itemOffsets ?? []).filter((entry) => entry.topOffset <= scrollY).at(-1)?.id === heading.slug}>
             <a href="#{heading.slug}">
                 {heading.text}
             </a>
         </li>
     {/each}
 </ol>
+
+<style>
+    li {
+        border-left: 4px solid transparent;
+    }
+    .active {
+        border-left: 4px solid var(--color-blue-dark);
+        background-color: var(--color-blue-light);
+    }
+    li:hover {
+        border-left: 4px solid var(--color-blue-light);
+        background-color: var(--color-blue-xlight);
+    }
+</style>
